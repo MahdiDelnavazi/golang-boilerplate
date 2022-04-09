965341f5d5ac2a021d38d06b23101994e3fd39c1
mahdidelnavazi <delnavazi.mahdi@gmail.com>
2022-04-09 13:28:43 +0300

    add test for PasetoMaker.go

---
+++ Middleware/token/PasetoMaker_test.go
@@ -0,0 +1,46 @@
+package token
+
+import (
+	"github.com/stretchr/testify/require"
+	"golang-boilerplate/Helper"
+	"testing"
+	"time"
+)
+
+func TestPasetoMaker(t *testing.T) {
+	maker, err := NewPasetoMaker(Helper.RandomString(32))
+	require.NoError(t, err)
+
+	username := Helper.RandomOwner()
+	duration := time.Minute
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
+}
+
+func TestExpiredPasetoToken(t *testing.T) {
+	maker, err := NewPasetoMaker(Helper.RandomString(32))
+	require.NoError(t, err)
+
+	token, err := maker.CreateToken(Helper.RandomOwner(), -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
